ddf0ac1aea4415e5ce9bb32b5d0732189a41318e
Sarushan Thiyakumar <sarushan.thiy@gmail.com>
2022-05-24 16:36:19 +0100

    C:/Program Files/Git/ removed from cityBeach data

--- controllers/locations.py
+++ controllers/locations.py
@@ -4,7 +4,7 @@ locations = [
     {
         "id": 1,
         "Name": "Ottawa",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Canada",
         "Continent": "North America",
         "Img": "https://content.r9cdn.net/rimg/dimg/66/6f/19cfcc07-city-9618-1629b990c4d.jpg?crop=true&width=1366&height=768&xhint=1491&yhint=1383" 
@@ -12,7 +12,7 @@ locations = [
     {
         "id": 2,
         "Name": "Ahmedabad",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "India",
         "Continent": "Asia",
         "Img": "https://www.tripsavvy.com/thmb/18jukZwmonkaOB7Gk2t_ua9GDYQ=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/sidi-bashir-mosque-in-ahmedabad-1322198135-18dddfc8f76e4a2f8cbce7d522ad4cb7.jpg"
@@ -20,7 +20,7 @@ locations = [
     {
         "id": 3,
         "Name": "Tiverton",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "United Kingdom",
         "Continent": "Europe",
         "Img": "https://eu-assets.simpleview-europe.com/southdevon2018/imageresizer/?image=%2Fdmsimgs%2FRiver_Exe_1_-_c_Dennis_Knowles_sm_1744811111.jpg&action=ProductDetailPro"        
@@ -28,7 +28,7 @@ locations = [
     {
         "id": 4,
         "Name": "Boulogne-sur-Mer",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "France",
         "Continent": "Europe",
         "Img": "https://media.istockphoto.com/photos/beautiful-landscape-of-the-coast-in-the-north-of-france-picture-id1180007907?k=20&m=1180007907&s=612x612&w=0&h=4qVTKD8Y71QQfS7PV6HFDFtgZlU6iLLP5ykQnSTf5uU="
@@ -36,7 +36,7 @@ locations = [
     {
         "id": 5,
         "Name": "Buffalo",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "United States",
         "Continent": "North America",
         "Img": "https://www.americancityandcounty.com/files/2021/11/Ele-Tower-Cirtscape-Sunset-Uncropped-2x3-small-1024x512.jpg"
@@ -44,7 +44,7 @@ locations = [
     {
         "id": 6,
         "Name": "Los Angeles",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "United States",
         "Continent": "North America",
         "Img": "https://www.gousa.in/sites/default/files/styles/hero_l/public/images/hero_media_image/2017-01/Getty_515070156_EDITORIALONLY_LosAngeles_HollywoodBlvd_Web72DPI_0.jpg?itok=lst_2_5d"        
@@ -52,7 +52,7 @@ locations = [
     {
         "id": 7,
         "Name": "Adeje",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Spain",
         "Continent": "Europe",
         "Img": "https://www.kyero.com/guides/wp-content/uploads/2018/12/xAdeje-beach-2400x1603.jpg.pagespeed.ic.anX56D5Pn_.jpg"
@@ -60,7 +60,7 @@ locations = [
     {
         "id": 8, 
         "Name": "Havana",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Cuba",
         "Continent": "South America",
         "Img": "https://media.gq.com/photos/5914dd2943572d096b80c8f3/4:3/w_1728,h_1296,c_limit/guide-to-old-havana-cuba.jpg"
@@ -68,7 +68,7 @@ locations = [
     {
         "id": 9, 
         "Name": "Alberta",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Canada",
         "Continent": "North America",
         "Img": "https://assets.justenergy.com/wp-content/uploads/2021/10/moving-to-alberta-canada-guide-and-faqs-image-of-lake.jpg"        
@@ -76,7 +76,7 @@ locations = [
     {
         "id": 10,
         "Name": "New York City",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "United States",
         "Continent": "North America",
         "Img": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-688899881-1519413300.jpg"
@@ -84,7 +84,7 @@ locations = [
     {
         "id": 11,
         "Name": "Penida Island",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Indonesia",
         "Continent": "Asia",
         "Img": "https://i0.wp.com/girleatworld.net/wp-content/uploads/2018/04/nusa-penida-kelingking-1.jpg?fit=1400%2C1116&ssl=1"
@@ -92,7 +92,7 @@ locations = [
     {
         "id": 12,
         "Name": "Cox's Bazaar",
-        "City/Beach": "Beach",	
+        "cityBeach": "Beach",	
         "Country": "Bangladesh",
         "Continent": "Asia",
         "Img": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/e2/f8/43/longest-sea-beach-in.jpg?w=700&h=-1&s=1"
@@ -100,7 +100,7 @@ locations = [
     {
         "id": 13,
         "Name": "Sharm-el-Sheikh",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Egypt",
         "Continent": "Africa",
         "Img": "https://media.istockphoto.com/photos/beach-at-sharm-el-sheikh-picture-id496255780?k=20&m=496255780&s=612x612&w=0&h=xyrZ8Aas2f0v4S1Zdz8U9KyZYIyppZMEWoB3nSh9CO4="
@@ -108,7 +108,7 @@ locations = [
     {
         "id": 14,
         "Name": "Copenhagen",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Denmark",
         "Continent": "Europe",
         "Img": "https://media.timeout.com/images/105659619/image.jpg"
@@ -116,7 +116,7 @@ locations = [
     {
         "id": 15,
         "Name": "Cabo San Lucas",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Mexico",
         "Continent": "South America",
         "Img": "https://www.planetware.com/wpimages/2018/09/mexico-cabo-san-lucas-things-to-do-sunset-lands-end.jpg"
@@ -124,7 +124,7 @@ locations = [
     {
         "id": 16,
         "Name": "Lagos",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Nigeria",
         "Continent": "Africa",
         "Img": "https://media.newyorker.com/photos/5909523dc14b3c606c103bac/16:9/w_1280,c_limit/Victoria-Island-580.jpg"
@@ -132,7 +132,7 @@ locations = [
     {
         "id": 17,
         "Name": "Unguja",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Tanzania",
         "Continent": "Africa",
         "Img": "https://photo620x400.mnstatic.com/6d8b6d2dfd308fa5b26d517a6bff7fa3/unguja-ukku.jpg"
@@ -140,7 +140,7 @@ locations = [
     {
         "id": 18,
         "Name": "Corwnall",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "United Kingdom",
         "Continent": "North America",
         "Img": "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/United%20Kingdom/Cornwall/cornwall-things-to-do-guide-xlarge.jpg"        
@@ -148,7 +148,7 @@ locations = [
     {
         "id": 19,
         "Name": "Majorca",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Spain",
         "Continent": "Europe",
         "Img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTn-TE55WwRGFWAhWMpsWDkEbp5Aew2M3gBQ&usqp=CAU"
@@ -156,7 +156,7 @@ locations = [
     {
         "id": 20,
         "Name": "Paris",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "France",
         "Continent": "Europe",
         "Img": "https://pix10.agoda.net/geo/city/15470/1_15470_02.jpg?ca=6&ce=1&s=1920x822"        
@@ -164,7 +164,7 @@ locations = [
     {
         "id": 21,
         "Name": "Positano",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Italy",
         "Continent": "Europe",
         "Img": "https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/3c/40/d0.jpg"
@@ -172,7 +172,7 @@ locations = [
     {
         "id": 22,
         "Name": "Mombasa",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Kenya",
         "Continent": "Africa",
         "Img": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/ff/d4/cb/mbabamb-3-largejpg.jpg?w=600&h=400&s=1"        
@@ -180,7 +180,7 @@ locations = [
     {
         "id": 23,
         "Name": "Rabat",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Morocco",
         "Continent": "Africa",
         "Img": "https://www.isdb.org/sites/default/files/styles/hero/public/image1.jpg?itok=TaECZ3uS"
@@ -188,7 +188,7 @@ locations = [
     {
         "id": 24,
         "Name": "Goa",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "India",
         "Continent": "Asia",
         "Img": "https://www.planetware.com/wpimages/2020/06/india-goa-best-beaches-colva-beach.jpg"        
@@ -196,7 +196,7 @@ locations = [
     {
         "id": 25,
         "Name": "Sentosa Island",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Singapore",
         "Continent": "Asia",
         "Img": "https://www.sentosa.com.sg/-/media/sentosa/article-listing/articles/2020/swimming-in-sentosa/swimmmingatsentosamain.jpg?revision=88869462-b375-48a5-91a7-07147f804532"        
@@ -204,7 +204,7 @@ locations = [
     {
         "id": 26,
         "Name": "Kuala Lumpur",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Malaysia",
         "Continent": "Asia",
         "Img": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/08/21/15/kuala-lumpur.jpg?quality=75&width=1200&auto=webp"
@@ -212,7 +212,7 @@ locations = [
     {
         "id": 27,
         "Name": "Tokyo",
-        "City/Beach": "City",
+        "cityBeach": "City",
         "Country": "Japan",
         "Continent": "Asia",
         "Img": "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002533/img/basic/a0002533_main.jpg?20210122155600&q=80&rw=750&rh=536"
@@ -220,7 +220,7 @@ locations = [
     {
         "id": 28,
         "Name": "Ishigaki",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Japan",
         "Continent": "Asia",
         "Img": "https://visitokinawajapan.com/wp-content/uploads/2021/11/de103_kv_ishigaki-island-scenery.jpg"
@@ -228,7 +228,7 @@ locations = [
     {
         "id": 29,
         "Name": "Sanya",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "China",
         "Continent": "Asia",
         "Img": "https://rccl-h.assetsadobe.com/is/image/content/dam/royal/data/ports/sanya-china/overview/sanya-china-beach.jpg?$750x320$"
@@ -236,7 +236,7 @@ locations = [
     {
         "id": 30,
         "Name": "Patong",
-        "City/Beach": "Beach",
+        "cityBeach": "Beach",
         "Country": "Thailand",
         "Continent": "Asia",
         "Img": "https://a.cdn-hotels.com/gdcs/production2/d1117/295d586d-6a84-415f-8551-0eb1deb169ba.jpg?impolicy=fcrop&w=800&h=533&q=medium"
@@ -289,4 +289,3 @@ def find_by_country(Country):
         return next( location for location in locations if location['Country'] == Country)
     except:
         raise BadRequest(f"Can't find the location by country {Country}")
-
